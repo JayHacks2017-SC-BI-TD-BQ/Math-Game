dcfb15418c09e3ed27d585a8abc207f729f09a90
rayyose7 <rayyose7@gmail.com>
2017-02-18 12:03:43 -0600

    GitEZPush.sh

--- Math_Game/mainwindow.cpp
+++ Math_Game/mainwindow.cpp
@@ -27,6 +27,9 @@ void MainWindow::on_btnAnswer_clicked()
 //int operand1, operand2, correctAnswer; //These are your integers.
 void MainWindow::generateEasyAdditionProblem(){
     //Generate random 2 operands and create a correctAnswer using them
+    operand1 = generateRandomNumber(1,100);
+    operand2 = generateRandomNumber(1,100);
+    correctAnswer = operand1+operand2;
 }
 void MainWindow::generateEasySubtractionProblem(){
     //Comment
