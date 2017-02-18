a5f3cd330bb023ae86f67ca50c41f2c3dfb4536b
Sherman Choi <think88act@gmail.com>
2017-02-18 08:23:50 -0700

    Basic UI constructed

---
+++ build-Math_Game-Desktop_Qt_5_8_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,78 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.8.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QPushButton *btnGenerateProblem;
+    QLabel *lblDisplay;
+    QToolBar *toolBar;
+    QMenuBar *menuBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(400, 200);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        btnGenerateProblem = new QPushButton(centralWidget);
+        btnGenerateProblem->setObjectName(QStringLiteral("btnGenerateProblem"));
+        btnGenerateProblem->setGeometry(QRect(10, 140, 150, 20));
+        lblDisplay = new QLabel(centralWidget);
+        lblDisplay->setObjectName(QStringLiteral("lblDisplay"));
+        lblDisplay->setGeometry(QRect(10, 10, 380, 20));
+        MainWindow->setCentralWidget(centralWidget);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QStringLiteral("toolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 400, 22));
+        MainWindow->setMenuBar(menuBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Math Game", Q_NULLPTR));
+        btnGenerateProblem->setText(QApplication::translate("MainWindow", "Generate Problem", Q_NULLPTR));
+        lblDisplay->setText(QApplication::translate("MainWindow", "Please generate a problem.", Q_NULLPTR));
+        toolBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
